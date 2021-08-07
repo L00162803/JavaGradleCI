f8db5bddf83ca0c7118390a6a6bfe5915d38fccc
Martin Connolly <martin.connolly@aspect.com>
2021-08-07 16:38:03 +0100

    initial commit

---
+++ app/src/test/java/GradleDemo/AppTest.java
@@ -0,0 +1,21 @@
+package GradleDemo;
+
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+
+public class AppTest{
+    @Test public void appHasAGreeting(){
+        //Arrange
+        App myApp = new App();
+        //Act and Assert
+        assertNotNull(myApp.getGreeting(), "app should have greeting");
+    }
+
+    @Test public void appFailedTest(){
+        //Arrange
+        App myApp = new App();
+        //Act and Assert
+        assertNull(myApp.getGreeting(), "app should have greeting");
+    }
+
+}
